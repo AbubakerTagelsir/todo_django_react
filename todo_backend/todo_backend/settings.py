0d9a5dfd94aa767b5eacf3252e63029f96b0ece3
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2020-11-02 03:25:37 +0400

    Django & React

--- todo_backend/todo_backend/settings.py
+++ todo_backend/todo_backend/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'todo',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,6 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CORS_ORIGIN_WHITELIST = (
+     'http://localhost:3000',
+ )
 
 
 # Internationalization
